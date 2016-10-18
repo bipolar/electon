f33f26fc6ef0f238997a434a68a87a39ae2e1407
csamarghitan <catalin.samarghitan@gmail.com>
2016-10-18 16:59:05 +0200

    Initial commit from the atomist bot

---
+++ src/main/java/lu/ksoft/electron/MyServiceApplication.java
@@ -0,0 +1,12 @@
+package lu.ksoft.electron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyServiceApplication.class, args);
+    }
+}
